413c130384d21d43666a21c11f4a9d58c44d0ba4
ylilac <ylilac@163.com>
2020-02-07 17:47:00 +0800

    增加swagger文档请求参数描述 增加model尾性名描述

--- src/main/java/com/yy/jpa/controller/UserController.java
+++ src/main/java/com/yy/jpa/controller/UserController.java
@@ -3,7 +3,9 @@ package com.yy.jpa.controller;
 import com.yy.jpa.model.User;
 import com.yy.jpa.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,34 +23,36 @@ public class UserController {
 
     @ApiOperation("删除")
     @GetMapping(value = "/delete/{id}")
-    public void delete(@PathVariable("id")int id){
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "int")
+    public void delete(@PathVariable("id") int id) {
         userService.delete(id);
     }
 
     @ApiOperation("新增")
     @PostMapping(value = "/add")
-    public void insert(@RequestBody User user){
+//    @ApiImplicitParam(name="user",value = "用户信息",required = true,dataType = "string")
+    public void insert(@RequestBody @ApiParam("新增用户信息") User user) {
         userService.insert(user);
     }
 
     @ApiOperation("修改")
     @PostMapping(value = "/update")
-    public void update(@RequestBody User user){
+    public void update(@RequestBody @ApiParam("修改用户信息") User user) {
         userService.update(user);
     }
 
     @ApiOperation("获取")
     @GetMapping(value = "/{id}/select")
-    public User select(@PathVariable("id")int id){
+    public User select(@PathVariable("id") @ApiParam("用户id") int id) {
         return userService.selectById(id);
     }
 
     @ApiOperation("分页")
     @GetMapping(value = "/selectAll/{pageNum}/{pageSize}")
-    public List<User> selectAll(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
+    public List<User> selectAll(@PathVariable("pageNum") @ApiParam("页码（从0开始）") int pageNum, @PathVariable("pageSize") @ApiParam("每页显示条数") int pageSize) {
         Iterator<User> userIterator = userService.selectAll(pageNum, pageSize);
         List<User> list = new ArrayList<>();
-        while(userIterator.hasNext()){
+        while (userIterator.hasNext()) {
             list.add(userIterator.next());
         }
         return list;
--- src/main/java/com/yy/jpa/model/User.java
+++ src/main/java/com/yy/jpa/model/User.java
@@ -1,6 +1,8 @@
 package com.yy.jpa.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -9,23 +11,29 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "tb_user")
+@ApiModel
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "主键")
     private int id;
 
     @Column(name="username")
+    @ApiModelProperty(value = "用户名")
     private String userName;
 
     @Column(name="password")
+    @ApiModelProperty(value = "密码")
     private String password;
 
     @Column(name="birthday")
+    @ApiModelProperty(value = "出生年月")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
     private Date birthday;
 
     @Column(name="createtime")
+    @ApiModelProperty(value = "创建时间")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
